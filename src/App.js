d96416daa59f74385381ca66a519af40cc5f0bf9
Gia1987 <giacomo.n27@gmail.com>
2018-07-26 20:52:19 +0100

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,63 +1,83 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Grid from './component/Grid.js'
+import Grid from './component/Grid/Grid.js'
+import Hoover from './component/Hoover/Hoover.js'
 
 class App extends Component {
   state= {
-    sizeGrid: 1,
-    x: 0,
-    y: 0
+    sizeGrid: 0,
+    x: null,
+    y: null
   }
 
+initialXHandler = (event) => {
+  this.setState( {
+    x: event.target.value
+  });
+}
+initialYHandler = (event) => {
+  this.setState( {
+    y: event.target.value
+  });
+}
   sizeGridHandler = () => {
     this.setState( {
       sizeGrid: this.state.sizeGrid + 1
     });
   }
-  rigthHandler = () => {
-    if(this.state.x < (this.state.sizeGrid - 1)){
-      this.setState( {
-        x: this.state.x + 1
-      });
+  incraseXHandler = () => {
+    this.setState( {
+      x: this.state.x + 1
+    });
+  }
+  decraseXHandler = () => {
+    this.setState( {
+      x: this.state.x - 1
+    });
+  }
+  incraseYHandler = () => {
+    this.setState( {
+      y: this.state.y + 1
+    });
+  }
+  decraseYHandler = () => {
+    this.setState( {
+      y: this.state.y - 1
+    });
+  }
 
-    }
+  updatedX = () =>{
+    return this.state.x
   }
-  leftHandler = () => {
-    if(this.state.x > 0){
-      this.setState( {
-        x: this.state.x - 1
-      });
-    }
-  }
-  upHandler = () => {
-    if(this.state.y < this.state.sizeGrid){
-      this.setState( {
-        y: this.state.y + 1
-      });
-    }
-  }
-  downHandler = () => {
-    if(this.state.y > 0){
-      this.setState( {
-        y: this.state.y - 1
-      });
-    }
+
+  updatedY = () =>{
+    return this.state.y
   }
   render() {
     return (
       <div className="App">
-      <h1>Hoover</h1>
+        <h1>Hoover</h1>
         <p>Position: {this.state.x}, {this.state.y}</p>
+        <input type="number" placeholder='Inital X' onChange= {this.initialXHandler.bind(this)}/>
+        <input type="number" placeholder='Inital Y' onChange= {this.initialYHandler.bind(this)}/>
         <button onClick = {this.sizeGridHandler} >
           Grid Size: {this.state.sizeGrid}x{this.state.sizeGrid}
         </button>
-        <p>{console.log(<tr id={`${this.state.y}`}><td className={`${this.state.x}`}></td></tr>)}</p>
-        < Grid sizeGrid ={this.state.sizeGrid}/>
-        <button onClick={this.leftHandler}>LEFT</button>
-        <button onClick={this.upHandler}>UP</button>
-        <button onClick={this.downHandler}>DOWN</button>
-        <button onClick={this.rigthHandler}>RIGHT</button>
+        < Grid
+          sizeGrid ={this.state.sizeGrid}
+          updatedX = {this.state.x}
+          updatedY = {this.state.y}
+        />
+        < Hoover
+          sizeGrid ={this.state.sizeGrid}
+          updatedX = {this.state.x}
+          updatedY = {this.state.y}
+          incraseX = {this.incraseXHandler}
+          decraseX = {this.decraseXHandler}
+          incraseY = {this.incraseYHandler}
+          decraseY = {this.decraseYHandler}
+        />
       </div>
     );
   }
--- src/component/Grid/Grid.js
+++ src/component/Grid/Grid.js
@@ -11,18 +11,27 @@ class Grid extends Component {
       let children = []
       //Inner loop to create children
       for (let j = 0; j < this.props.sizeGrid; j++) {
-        children.push(<td className= {`${j}`}></td>)
+        children.push(<td key ={`${j}`}>
+          <img
+            src='https://target.scene7.com/is/image/Target/52731060_Alt01?wid=488&hei=488&fmt=pjpeg'
+            width="100" height="100"
+            style={(this.props.updatedX == j) && (this.props.updatedY == i ) ? {visibility: 'visible'} : {visibility: 'hidden'}}
+          />
+          </td>)
       }
       //Create the parent and add the children
-      table.push(<tr id={`${i}`}>{children}</tr>)
+      table.push(<tr key ={`${i}`}>{children}</tr>)
     }
-    return table
+    return table.reverse()
   }
+
   render() {
   return (
     <div>
       <table>
-        {this.createTable()}
+        <tbody>
+          {this.createTable()}
+        </tbody>
       </table>
     </div>
   );
